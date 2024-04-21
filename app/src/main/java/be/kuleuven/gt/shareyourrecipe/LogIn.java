58ef12c1bef8432a09d01c10f681b039b4dbf6bc
Viktoriya <viktoriya.postulga@student.kuleuven.be>
2024-04-21 22:17:32 +0300

    logIn0.3

--- app/src/main/java/be/kuleuven/gt/shareyourrecipe/LogIn.java
+++ app/src/main/java/be/kuleuven/gt/shareyourrecipe/LogIn.java
@@ -1,6 +1,8 @@
 package be.kuleuven.gt.shareyourrecipe;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LogIn extends AppCompatActivity {
+    private Button btnSignUp;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+    }
+
+    ///action to happen when SignUp clicked
+    public void onBtnSignUp_Clicked(View Caller){
+
+    }
+
+    ///action to happen when Submit clicked
+    public void onBtnSubmit_Clicked(View Caller){
+
     }
 }
